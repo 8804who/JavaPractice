a94266c92ee8c71bec0a72a26f82214f7ed5071d
1a2a3a4a5 <8804who@naver.com>
2021-10-12 21:44:59 +0900

    Java

--- src/백준연습/백준_2304번_창고다각형.java
+++ src/백준연습/백준_2304번_창고다각형.java
@@ -8,9 +8,9 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class 백준_2304번_창고다각형 {
-    private static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
     static void input(int[][] arr, int i) throws IOException {
-        StringTokenizer st;
         st=new StringTokenizer(br.readLine());
         arr[i][0]=Integer.parseInt(st.nextToken());
         arr[i][1]=Integer.parseInt(st.nextToken());
@@ -28,21 +28,20 @@ public class 백준_2304번_창고다각형 {
         });
     }
 
-    static int find_max(int[][] arr){
-        int max=0;
+    static int find_pivot(int[][] arr){
+        int pivot=0;
         for(int i=0;i<arr.length;i++){
-            if (arr[max][1]<arr[i][1]) {
-                max=i;
+            if (arr[pivot][1]<arr[i][1]) {
+                pivot=i;
             }
         }
-        return max;
+        return pivot;
     }
 
     public static void main(String[] args) throws IOException {
-
         int N=Integer.parseInt(br. readLine());
         int[][] arr=new int[N][2];
-        int max=0, pivot = 0, sum=0, height=0;
+        int pivot = 0, sum=0, height=0;
 
         for(int i=0;i<N;i++){
             input(arr, i);
@@ -50,15 +49,14 @@ public class 백준_2304번_창고다각형 {
 
         sort(arr);
 
-
-        pivot=find_max(arr);
-        max=arr[pivot][1];
+        pivot=find_pivot(arr);
 
         height=arr[0][1];
         for (int i=1;i<=pivot;i++){
             sum+=height*(arr[i][0]-arr[i-1][0]);
             if (height<arr[i][1]) height=arr[i][1];
         }
+
         sum+=arr[pivot][1];
 
         height=arr[N-1][1];
@@ -66,6 +64,7 @@ public class 백준_2304번_창고다각형 {
             sum+=height*(arr[i+1][0]-arr[i][0]);
             if(height<arr[i][1]) height=arr[i][1];
         }
+
         System.out.print(sum);
     }
 }
