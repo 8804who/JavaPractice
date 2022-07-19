f66c67dabe317260086ffee41a67cb3b23aa327f
1a2a3a4a5 <8804who@naver.com>
2022-07-19 14:44:44 +0900

    백준문제풀이

---
+++ src/못푼문제/백준_1806번_부분합.java
@@ -0,0 +1,34 @@
+package 못푼문제;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class 백준_1806번_부분합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+
+        int l = 0;
+        int sum=0;
+        int min = 100001;
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++) {
+            nums[i]=Integer.parseInt(st.nextToken());
+            sum+=nums[i];
+            while(sum>=M && l<=i){
+                min=Math.min(min,i-l+1);
+                sum -= nums[l];
+                l++;
+            }
+        }
+        if(min!=100001) System.out.print(min);
+        else System.out.print(0);
+    }
+}
