2c935ac55dd1836cef9a5a78bebd2b78e28ec509
1a2a3a4a5 <8804who@naver.com>
2022-07-29 12:33:23 +0900

    백준문제풀이

---
+++ src/백준연습/백준_7579번_앱.java
@@ -0,0 +1,53 @@
+package 백준연습;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class 백준_7579번_앱 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int min = 10001;
+        int[] memory = new int[N];
+        int[] restart = new int[N];
+
+        int[][] dp = new int[N][N*100+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<N; i++) {
+            memory[i]=Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<N; i++) {
+            restart[i]=Integer.parseInt(st.nextToken());
+        }
+
+
+        for(int i=0;i<N;i++){
+           for(int j=0;j<=N*100;j++){
+               if(i==0) {
+                   if(j-restart[i]>=0) dp[i][j]=memory[i];
+               }
+               else{
+                   if(j-restart[i]>=0) dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-restart[i]]+memory[i]);
+                   else dp[i][j]=dp[i-1][j];
+               }
+           }
+
+           for(int j=0;j<=N*100;j++){
+               if(dp[i][j]>=M) {
+                   min = Math.min(min, j);
+               }
+            }
+        }
+        System.out.print(min);
+    }
+}
